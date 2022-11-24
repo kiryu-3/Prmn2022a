1ea052360c3ef9e356bafbfbbc85ce3091ffeb75
kiryu-3 <segkao.galaxy@gmail.com>
2022-11-25 08:41:40 +0900

    221125 commit

--- src/lecture04/ATM.java
+++ src/lecture04/ATM.java
@@ -1,4 +1,76 @@
 package lecture04;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ATM {
+    private List<Account> accountList ;
+    private int index;
+
+    Scanner input = new Scanner(System.in);
+
+    public ATM(){
+        accountList = new ArrayList<>();
+    }
+
+    public void registerAccount(String name,String number){
+        accountList.add(new Account(name,number));
+        System.out.println(name+" さんのアカウントを口座番号：" +number+" で登録しました。");
+    }
+
+    public boolean exitstsAccount(String name,String number){
+        int checkpoint = 0;
+        for(Account alist : accountList){
+            String str = alist.getName();
+            String num = alist.getNumber();
+            if(str.equals(name)&&num.equals(number)){
+                index=accountList.indexOf(alist);
+                checkpoint++;
+            }
+        }
+        return checkpoint>0;
+    }
+
+    public void inOut(){
+        while(true){
+            System.out.println("1:入金 2:引出 3:終了");
+            int checheker = input.nextInt();
+            if(checheker==1){
+                System.out.println("いくら入金しますか");
+                deposit();
+            }else if(checheker==2){
+                System.out.println("いくら引き出しますか");
+                long minus = input.nextLong();
+                if(accountList.get(index).getBalance()-minus>=0){
+                    long money = withdraw(accountList.get(index).getBalance(),minus);
+                    accountList.get(index).setBalance(money);
+                }else{
+                    System.out.print("口座番号："+accountList.get(index).getNumber() +" から "+minus+"円引き出せませんでした。");
+                    System.out.println("残金:" + accountList.get(index).getBalance() + "円です。");
+                }
+            }else if(checheker==3){
+                System.out.println("終了します");
+                break;
+            }else{
+                System.out.println("ERROR!");
+            }
+        }
+
+    }
+
+    public void deposit(){
+        long plus = input.nextLong();
+        long sum = accountList.get(index).getBalance()+plus;
+        System.out.println("口座番号："+accountList.get(index).getNumber() +" に "+plus+"円入金しました。");
+        accountList.get(index).setBalance(sum);
+    }
+
+    public long withdraw(long nowmoney,long minus){
+        long sum = nowmoney-minus;
+        accountList.get(index).setBalance(sum);
+        System.out.print("口座番号："+accountList.get(index).getNumber() +" から "+minus+"円引き出しました。");
+        System.out.println("残金:" + sum + "円です。");
+        return sum;
+    }
 }
--- src/lecture04/Exercise4_2.java
+++ src/lecture04/Exercise4_2.java
@@ -1,8 +1,42 @@
-/*package lecture04;
+package lecture04;
 
+import java.util.Scanner;
+import java.util.Random;
 public class Exercise4_2 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         ATM atm = new ATM();
-        Account account = new
+        while(true){
+            System.out.println("1:登録　2:ログイン 3:終了");
+            int checker = input.nextInt();
+            if(checker==1){
+                System.out.println("名前を入力してください");
+                String name = input.next();
+                atm.registerAccount(name,numberDecide());
+            }else if(checker==2){
+                System.out.println("名前を入力してください");
+                String name = input.next();
+                System.out.println("口座番号を入力してください");
+                String number = input.next();
+                if(atm.exitstsAccount(name,number)){
+                    System.out.println("名前:"+name+" 口座番号："+number +" は存在します");
+                    atm.inOut();
+                }else {
+                    System.out.println("名前:" + name + " 口座番号：" + number + " は存在しません");
+                }
+            }else if(checker==3){
+                System.out.println("終了します");
+                break;
+            }else{
+                System.out.println("Error!");
+            }
+        }
+
     }
-}*/
+    public static String numberDecide(){
+        Random rand = new Random();
+        int num = rand.nextInt(89999) + 10000;
+        return String.valueOf(num);
+    }
+}
